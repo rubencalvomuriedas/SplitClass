32ff715d0aae9d2046a5b72ea14b16112569eb07
DESKTOP-JQ9OQ1I\Rubén <rcalvom2501@educantabria.es>
2026-05-07 22:48:47 +0200

    07/05/2026 Haciendo la portada, Puesto 2 botones, imagenes y el logo

--- SlipClass_Demo_1/src/main/java/org/example/slipclass_demo_1/HelloApplication.java
+++ SlipClass_Demo_1/src/main/java/org/example/slipclass_demo_1/HelloApplication.java
@@ -13,24 +13,24 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1620, 880);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        SQLDataAccess dataAccess = new SQLDataAccess();
-        Connection conn = SQLDataAccess.getConnection();
-        if (conn != null) {
-            System.out.println("Conexión exitosa a la base de datos");
-            launch(args);
-        } else {
-            System.out.println("Error al conectar a la base de datos");
-        }
-
-
-    }
+//    public static void main(String[] args) {
+//        SQLDataAccess dataAccess = new SQLDataAccess();
+//        Connection conn = SQLDataAccess.getConnection();
+//        if (conn != null) {
+//            System.out.println("Conexión exitosa a la base de datos");
+//            launch(args);
+//        } else {
+//            System.out.println("Error al conectar a la base de datos");
+//        }
+//
+//
+//    }
 
 
 }
