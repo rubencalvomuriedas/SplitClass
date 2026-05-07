ad8d213da609db0f8500c70a1a31f27c03e2b09a
COMPU-S027\daw1 <rubencalvomuriedas@gmail.com>
2026-05-07 09:51:07 +0200

    Empezando la Portada

--- SlipClass_Demo_1/src/main/java/org/example/slipclass_demo_1/HelloApplication.java
+++ SlipClass_Demo_1/src/main/java/org/example/slipclass_demo_1/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1620, 880);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
