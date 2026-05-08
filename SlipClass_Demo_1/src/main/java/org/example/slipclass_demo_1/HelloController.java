506ae019667ec78f8bd27b07c82add77f3502fba
COMPU-S022\daw1 <dgomeza01@educantabria.es>
2026-05-08 09:29:56 +0200

    no message

--- SlipClass_Demo_1/src/main/java/org/example/slipclass_demo_1/HelloController.java
+++ SlipClass_Demo_1/src/main/java/org/example/slipclass_demo_1/HelloController.java
@@ -1,7 +1,15 @@
 package org.example.slipclass_demo_1;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class HelloController {
 
@@ -19,12 +27,12 @@ public class HelloController {
         Alert alerta = new Alert(Alert.AlertType.INFORMATION);
         alerta.setTitle("Sección en construcción");
         alerta.setHeaderText(null);
-        // El mensaje que me has pedido
         alerta.setContentText("Próximamente...");
         alerta.show();
     }
 
     @FXML
+<<<<<<< HEAD
     public void clickSobreBuscar() {
         Alert alerta = new Alert(Alert.AlertType.INFORMATION);
         alerta.setTitle("Esto es lo que se va a mostar");
@@ -76,5 +84,24 @@ public class HelloController {
         alerta.setHeaderText(null);
         alerta.setContentText("Esto es lo que se va a mostar");
         alerta.show();
+=======
+    protected void onRegistrarseClick(ActionEvent event) {
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getResource("registro.fxml"));
+
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+
+            Scene scene = new Scene(root);
+
+
+            stage.setScene(scene);
+
+        } catch (IOException e) {
+            System.err.println("No se pudo cargar el formulario: " + e.getMessage());
+        }
+>>>>>>> 1e867d724088a601dfa96939148db4542badd6f2
     }
 }
