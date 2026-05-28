8cb5f0db9d71717f62a89bf3ff72ed17bab546e9
COMPU-S027\daw1 <rubencalvomuriedas@gmail.com>
2026-05-28 09:21:11 +0200

    registroGrupo

--- SlipClass_Demo_1/src/main/java/org/example/slipclass_demo_1/HelloApplication.java
+++ SlipClass_Demo_1/src/main/java/org/example/slipclass_demo_1/HelloApplication.java
@@ -14,18 +14,9 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<< HEAD
-
-  
-
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Grupos_view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
-
-=======
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menuPrincipal.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
->>>>>>> adef906beecdae2b9ec148f587a468269785d1cf
         stage.setTitle("SplitClass");
         stage.setScene(scene);
         stage.show();
